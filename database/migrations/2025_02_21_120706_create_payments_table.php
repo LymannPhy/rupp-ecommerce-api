f0febe0696d67951ffb34e8ddacb0a93e0bf37cd
lymann <phplaravel576@gmail.com>
2025-03-10 11:46:27 +0700

    update submit order method

--- database/migrations/2025_02_21_120706_create_payments_table.php
+++ database/migrations/2025_02_21_120706_create_payments_table.php
@@ -14,12 +14,15 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->nullable()->constrained()->onDelete('cascade'); 
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('payment_method')->default('qr_code');
             $table->string('transaction_hash')->unique(); 
             $table->string('from_account_id'); 
             $table->string('to_account_id');
             $table->string('currency', 10)->default('USD');
             $table->decimal('amount', 10, 2); 
-            $table->string('payment_status')->default('paid');
+            $table->string('status')->default('paid');
+            $table->string('md5_hash')->unique();
             $table->string('transaction_place')->nullable();
             $table->timestamps();
         });
